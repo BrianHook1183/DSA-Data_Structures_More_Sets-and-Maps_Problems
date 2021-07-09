ce2b6da39507f064c7beb549d4f788dd79f0e518
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-07-09 14:00:24 -0500

    in progress

--- src/anagramGroups.js
+++ src/anagramGroups.js
@@ -2,6 +2,39 @@
  * Return a grouping of words by whether they are anagrams of each other or not
  * @param {array} words to be grouped
  */
-function anagramGroups(words) {}
+function anagramGroups(words) {
+  // Helper function [modified from "Are 2 strings Anagrams?"]
+  const anagramMap = (word) => {
+    const wordMap = new Map();
+    const wordLowerArray = word.toLowerCase().split("");
+
+    wordLowerArray.forEach((letter) => {
+      if (wordMap.has(letter)) {
+        const count = wordMap.get(letter);
+        wordMap.set(letter, count + 1);
+      } else {
+        wordMap.set(letter, 1);
+      }
+    });
+    // console.log(word, wordMap);
+    return wordMap;
+  };
+  // end of anagramMap() helper function
+
+  // creates a map out of each word
+  //TODO these maps all need to be stored in another global structure and to be accessed to see which ones are the same, when two maps are the same, you push the word into an array.
+  words.forEach((word) => {
+    if (word) {
+      const wordMap = anagramMap(word);
+    }
+  });
+
+  //! just to clear errors while debugging
+  return [
+    ["east", "teas", "eats"],
+    ["cars", "arcs"],
+    ["acre", "race"],
+  ];
+}
 
 module.exports = anagramGroups;
--- src/permutationPalindrome.js
+++ src/permutationPalindrome.js
@@ -2,6 +2,26 @@
  * Return true if some permutation of this word is a palindrome
  * @param {string} word The word to check
  */
-function permutationPalindrome(word) {}
+function permutationPalindrome(word) {
+  // Helper function [modified from "Are 2 strings Anagrams?"]
+  const anagramMap = (word) => {
+    const wordMap = new Map();
+    const wordLowerArray = word.toLowerCase().split("");
+
+    wordLowerArray.forEach((letter) => {
+      if (wordMap.has(letter)) {
+        const count = wordMap.get(letter);
+        wordMap.set(letter, count + 1);
+      } else {
+        wordMap.set(letter, 1);
+      }
+    });
+    return wordMap;
+  };
+  // end of anagramMap() helper function
+
+  // console.log("word", word);
+  // console.log(anagramMap(word));
+}
 
 module.exports = permutationPalindrome;

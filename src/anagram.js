d4163749508e22036392fd71a566f523710ff049
Brian Hook <34469882+BrianHook1183@users.noreply.github.com>
2021-07-09 13:59:52 -0500

    passing tests

--- src/anagram.js
+++ src/anagram.js
@@ -3,6 +3,36 @@
  * @param {string} s1
  * @param {string} s2
  */
-function anagram(s1, s2) {}
+function anagram(s1, s2) {
+  s1Map = new Map();
+  s2Map = new Map();
+
+  const s1LowerArray = s1.toLowerCase().split("");
+  const s2LowerArray = s2.toLowerCase().split("");
+
+  s1LowerArray.forEach((letter) => {
+    if (s1Map.has(letter)) {
+      const count = s1Map.get(letter);
+      s1Map.set(letter, count + 1);
+    } else {
+      s1Map.set(letter, 1);
+    }
+  });
+  s2LowerArray.forEach((letter) => {
+    if (s2Map.has(letter)) {
+      const count = s2Map.get(letter);
+      s2Map.set(letter, count + 1);
+    } else {
+      s2Map.set(letter, 1);
+    }
+  });
+
+  //* equality check
+  for (let [key, value] of s1Map.entries()) {
+    if (s2Map.get(key) !== value) return false;
+  }
+
+  return true;
+}
 
 module.exports = anagram;
--- src/firstSingleCharacter.js
+++ src/firstSingleCharacter.js
@@ -2,6 +2,40 @@
  * Return the first character in the string that occurs only once.
  * @param {string} word the string to be analysed
  */
-function firstSingleCharacter(word) {}
+function firstSingleCharacter(word) {
+  if (!word.length) return null;
+
+  // Helper function [modified from "Are 2 strings Anagrams?"]
+  const anagramMap = (word) => {
+    // console.log("word", word);
+    const wordMap = new Map();
+    const wordLowerArray = word.toLowerCase().split("");
+    // console.log("wordLowerArray", wordLowerArray);
+
+    wordLowerArray.forEach((letter) => {
+      if (wordMap.has(letter)) {
+        const count = wordMap.get(letter);
+        wordMap.set(letter, count + 1);
+      } else {
+        wordMap.set(letter, 1);
+      }
+    });
+    // console.log("wordMap", wordMap);
+    return wordMap;
+  };
+  // end of anagramMap() helper function
+
+  const wordMap = anagramMap(word);
+  const lettersArray = word.toLowerCase().split("");
+
+  for (let letter of lettersArray) {
+    if (wordMap.has(letter)) {
+      if (wordMap.get(letter) === 1) {
+        return letter;
+      }
+    }
+  }
+  return null;
+}
 
 module.exports = firstSingleCharacter;
